0a26ac54b6b6fe89849ee6fa1c233a540e9507de
kapinskil <kapinski.l@gmail.com>
2019-05-21 22:24:31 +0200

    1st commit

---
+++ McvMovie/Controllers/MyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace McvMovie.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+    }
+}
