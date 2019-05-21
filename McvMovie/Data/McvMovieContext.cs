c29b575cf724b843c903d3f7f2a4a55cd39c69b0
kapinskil <kapinski.l@gmail.com>
2019-05-21 22:57:43 +0200

    Entity adding

---
+++ McvMovie/Data/McvMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace McvMovie.Models
+{
+    public class McvMovieContext : DbContext
+    {
+        public McvMovieContext (DbContextOptions<McvMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<McvMovie.Models.Movie> Movie { get; set; }
+    }
+}
--- McvMovie/Startup.cs
+++ McvMovie/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using McvMovie.Models;
 
 namespace McvMovie
 {
@@ -33,6 +35,9 @@ namespace McvMovie
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<McvMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("McvMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
